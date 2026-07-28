92bb27f2bbe454720d8751f6cd5f17c2e7ccd221
JinmuGo <jingmuio@gmail.com>
2026-07-28 21:55:56 +0900

    chore: convert commitlint config to commonjs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,2 @@
-export default { extends: ['@commitlint/config-conventional'] }
+// biome-ignore lint/style/noCommonJs: commitlint 설정 파일은 CJS 유지
+module.exports = { extends: ['@commitlint/config-conventional'] }
