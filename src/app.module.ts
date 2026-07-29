03d469951a2ac30c4b0165d1750733ca2a91b4f6
SHNAME <tlgud119@gmail.com>
2026-07-29 15:58:30 +0900

    feat(config): connect typeorm to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { validateEnv } from './config/env'
+import { type Env, validateEnv } from './config/env'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validate: validateEnv,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService<Env, true>) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST', { infer: true }),
+        port: config.get('DB_PORT', { infer: true }),
+        username: config.get('DB_USERNAME', { infer: true }),
+        password: config.get('DB_PASSWORD', { infer: true }),
+        database: config.get('DB_DATABASE', { infer: true }),
+        entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+        synchronize: config.get('NODE_ENV', { infer: true }) !== 'production',
+      }),
     }),
   ],
   controllers: [AppController],
