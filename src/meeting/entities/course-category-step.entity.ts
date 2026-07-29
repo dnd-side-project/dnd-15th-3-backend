3995951133cc75b9116b58223d46cf28a811509a
SHNAME <tlgud119@gmail.com>
2026-07-29 21:48:30 +0900

    feat(meeting): add course category step entity

---
+++ src/meeting/entities/course-category-step.entity.ts
@@ -0,0 +1,19 @@
+import { Category } from 'src/category/entities/category.entity'
+import { BaseEntity } from 'src/common/entities/base.entity'
+import { Column, Entity, JoinColumn, ManyToOne, Unique } from 'typeorm'
+import { Meeting } from './meeting.entity'
+
+@Entity()
+@Unique(['meeting', 'order'])
+export class CourseCategoryStep extends BaseEntity {
+  @ManyToOne(() => Meeting, { nullable: false })
+  @JoinColumn({ name: 'meeting_id' })
+  meeting: Meeting
+
+  @ManyToOne(() => Category, { nullable: false })
+  @JoinColumn({ name: 'category_id' })
+  category: Category
+
+  @Column()
+  order: number
+}
