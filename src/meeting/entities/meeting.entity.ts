819baef3dd082855eff5f5843f8bed0191d5bf3b
SHNAME <tlgud119@gmail.com>
2026-07-29 20:13:07 +0900

    feat(meeting): add meeting entity

---
+++ src/meeting/entities/meeting.entity.ts
@@ -0,0 +1,34 @@
+import { BaseEntity } from 'src/common/entities/base.entity'
+import { Place } from 'src/place/entities/place.entity'
+import { Check, Column, Entity, JoinColumn, ManyToOne } from 'typeorm'
+import { MeetingType } from './meeting-type.entity'
+
+@Entity()
+export class Meeting extends BaseEntity {
+  @ManyToOne(() => MeetingType, { nullable: false })
+  @JoinColumn({ name: 'meeting_type_id' })
+  meetingType: MeetingType
+
+  @ManyToOne(() => Place, { nullable: false })
+  @JoinColumn({ name: 'first_location_place_id' })
+  firstLocationPlace: Place
+
+  @Check(`length("name") >= 1`)
+  @Column({ length: 10 })
+  name: string
+
+  @Column('date')
+  date: string
+
+  @Column('time')
+  time: string
+
+  @Column({ unique: true })
+  accessToken: string
+
+  @Column({ nullable: true, unique: true })
+  routeImageKey: string
+
+  @Column({ nullable: true })
+  routeImageUploadedAt: Date
+}
