7ca931a6f802f9f4845aed14cb76a86d5bb3237e
SHNAME <tlgud119@gmail.com>
2026-07-29 21:40:59 +0900

    feat(meeting): add meeting participant entity and role enum

---
+++ src/meeting/entities/meeting-participant.entity.ts
@@ -0,0 +1,24 @@
+import { BaseEntity } from 'src/common/entities/base.entity'
+import { User } from 'src/user/entities/user.entity'
+import { Check, Column, Entity, JoinColumn, ManyToOne, Unique } from 'typeorm'
+import { ParticipantRole } from '../enums/participant-role.enum'
+import { Meeting } from './meeting.entity'
+
+@Entity()
+@Unique(['meeting', 'user'])
+export class MeetingParticipant extends BaseEntity {
+  @ManyToOne(() => Meeting, { nullable: false })
+  @JoinColumn({ name: 'meeting_id' })
+  meeting: Meeting
+
+  @ManyToOne(() => User, { nullable: false })
+  @JoinColumn({ name: 'user_id' })
+  user: User
+
+  @Column({ type: 'enum', enum: ParticipantRole })
+  role: ParticipantRole
+
+  @Check(`length("nickname") >= 1`)
+  @Column({ length: 10 })
+  nickname: string
+}
