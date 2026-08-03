e779ee5ff2a09ac5c8599e5fe31664478fa02d61
SHNAME <tlgud119@gmail.com>
2026-08-03 20:29:25 +0900

    fix(course): prevent duplicate place recommendations per meeting

--- src/course/entities/meeting-place-recommendation.entity.ts
+++ src/course/entities/meeting-place-recommendation.entity.ts
@@ -2,9 +2,10 @@ import { BaseEntity } from 'src/common/entities/base.entity'
 import { Meeting } from 'src/meeting/entities/meeting.entity'
 import { MeetingParticipant } from 'src/meeting/entities/meeting-participant.entity'
 import { Place } from 'src/place/entities/place.entity'
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm'
+import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm'
 
 @Entity()
+@Index(['meeting', 'place'], { unique: true })
 export class MeetingPlaceRecommendation extends BaseEntity {
   @ManyToOne(() => Meeting, { nullable: false, onDelete: 'CASCADE' })
   @JoinColumn({ name: 'meeting_id' })
