48fab85748165cfe320f4898a683d238cfd4dac8
SHNAME <tlgud119@gmail.com>
2026-08-03 15:42:31 +0900

    refactor(place): replace ImageBase with shared File entity

--- src/common/entities/image-base.entity.ts
+++
@@ -1,7 +0,0 @@
-import { Column } from 'typeorm'
-import { BaseEntity } from './base.entity'
-
-export abstract class ImageBase extends BaseEntity {
-  @Column({ unique: true })
-  imageKey: string
-}
