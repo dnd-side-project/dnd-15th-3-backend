785b6d50659de2a14fd8277dbd0e594be754c5fb
SHNAME <tlgud119@gmail.com>
2026-08-03 20:54:43 +0900

    refactor(course): remove like/dislike counters from MeetingPlaceRecommendation

--- src/course/entities/meeting-place-recommendation.entity.ts
+++ src/course/entities/meeting-place-recommendation.entity.ts
@@ -2,7 +2,7 @@ import { BaseEntity } from 'src/common/entities/base.entity'
 import { Meeting } from 'src/meeting/entities/meeting.entity'
 import { MeetingParticipant } from 'src/meeting/entities/meeting-participant.entity'
 import { Place } from 'src/place/entities/place.entity'
-import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm'
+import { Entity, Index, JoinColumn, ManyToOne } from 'typeorm'
 
 @Entity()
 @Index(['meeting', 'place'], { unique: true })
@@ -18,10 +18,4 @@ export class MeetingPlaceRecommendation extends BaseEntity {
   @ManyToOne(() => MeetingParticipant, { nullable: false })
   @JoinColumn({ name: 'recommended_by' })
   recommendedBy: MeetingParticipant
-
-  @Column({ default: 0 })
-  likeCount: number
-
-  @Column({ default: 0 })
-  dislikeCount: number
 }
