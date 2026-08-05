fbb26048d6a741c51c265e72f7b8043bc7b11b4d
SHNAME <tlgud119@gmail.com>
2026-08-05 15:21:42 +0900

    feat(common): add BaseException as common domain excpetion base

---
+++ src/common/exception/base.exception.ts
@@ -0,0 +1,11 @@
+import { ErrorCode, ValidationErrorDetail } from './error-code.type'
+
+// 도메인 예외의 공통 상위 타입. 도메인은 이 클래스를 상속한 예외 하나만 두고, 개별 실패는 ErrorCode로 구분
+export abstract class BaseException extends Error {
+  protected constructor(
+    readonly errorCode: ErrorCode,
+    readonly errors?: ValidationErrorDetail[],
+  ) {
+    super(errorCode.message)
+  }
+}
