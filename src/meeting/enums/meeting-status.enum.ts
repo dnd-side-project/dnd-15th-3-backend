2414a769e4a087e73d83408e6fc55dff23f41035
White_DeepDive <165636120+SHNAME@users.noreply.github.com>
2026-08-05 16:11:32 +0900

    feat(meeting): add Meeting status field to Meeting entity   (#21)
    * feat(meeting): add meeting status enum
    
    * feat(meeting): add status field to Meeting entity

---
+++ src/meeting/enums/meeting-status.enum.ts
@@ -0,0 +1,7 @@
+export enum MeetingStatus {
+  RecommendationCollecting = 'RECOMMENDATION_COLLECTING',
+  CourseGenerating = 'COURSE_GENERATING',
+  CourseGenerated = 'COURSE_GENERATED',
+  CourseGenerationFailed = 'COURSE_GENERATION_FAILED',
+  CourseConfirmed = 'COURSE_CONFIRMED',
+}
