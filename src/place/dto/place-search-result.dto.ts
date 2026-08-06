c9416e38753edc8172f5a2938f0e5b496bf58195
SHNAME <tlgud119@gmail.com>
2026-08-06 23:20:11 +0900

    refactor(place): simplify primaryImageUrl example

--- src/place/dto/place-search-result.dto.ts
+++ src/place/dto/place-search-result.dto.ts
@@ -13,10 +13,6 @@ export class PlaceSearchResultDto {
   @ApiProperty({ description: '주소', example: '서울 성동구 성수이로 1' })
   address: string
 
-  @ApiProperty({
-    description: '대표 사진 URL',
-    example:
-      'https://axxxxxxxxxxx.compat.objectstorage.ap-hyderabad-1.oraclecloud.com/momo-bucket-dev/places/cafe-momo.png',
-  })
+  @ApiProperty({ description: '대표 사진 URL', example: 'https://...' })
   primaryImageUrl: string
 }
