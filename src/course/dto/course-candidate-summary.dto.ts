9958265dee7b798a4a61b30d2caca608c61c223d
SHNAME <tlgud119@gmail.com>
2026-08-08 16:06:26 +0900

    feat(course): add order field to course candidate summary

--- src/course/dto/course-candidate-summary.dto.ts
+++ src/course/dto/course-candidate-summary.dto.ts
@@ -7,4 +7,10 @@ export class CourseCandidateSummaryDto {
     pattern: '^\\d+$',
   })
   courseCandidateId!: string
+
+  @ApiProperty({
+    description: '코스 후보 순서',
+    example: 1,
+  })
+  order!: number
 }
