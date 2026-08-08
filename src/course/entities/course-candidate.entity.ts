f5084ce6de652eb6ce9352c409cf70e417abb7c4
SHNAME <tlgud119@gmail.com>
2026-08-08 20:23:08 +0900

    feat(course): add name field to course candidate entity

--- src/course/entities/course-candidate.entity.ts
+++ src/course/entities/course-candidate.entity.ts
@@ -1,9 +1,10 @@
 import { BaseEntity } from 'src/common/entities/base.entity'
 import { Meeting } from 'src/meeting/entities/meeting.entity'
-import { Column, Entity, JoinColumn, ManyToOne, Unique } from 'typeorm'
+import { Check, Column, Entity, JoinColumn, ManyToOne, Unique } from 'typeorm'
 
 @Entity()
 @Unique(['meeting', 'order'])
+@Check(`length("name") >= 1`)
 export class CourseCandidate extends BaseEntity {
   @ManyToOne(() => Meeting, { nullable: false, onDelete: 'CASCADE' })
   @JoinColumn({ name: 'meeting_id' })
@@ -11,6 +12,9 @@ export class CourseCandidate extends BaseEntity {
 
   @Column()
   order: number
+
+  @Column({ type: 'varchar', length: 15 })
+  name: string
 
   @Column({ default: false })
   isSelected: boolean
