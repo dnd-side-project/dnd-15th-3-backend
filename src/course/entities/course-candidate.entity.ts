544733805b60f0d63206eaf771d73e1f712bf1e1
SHNAME <tlgud119@gmail.com>
2026-08-11 10:24:44 +0900

    feat(course): 모임당 확정 코스를 1개로 제한하는 제약을 추가한다

--- src/course/entities/course-candidate.entity.ts
+++ src/course/entities/course-candidate.entity.ts
@@ -1,9 +1,18 @@
 import { BaseEntity } from 'src/common/entities/base.entity'
 import { Meeting } from 'src/meeting/entities/meeting.entity'
-import { Check, Column, Entity, JoinColumn, ManyToOne, Unique } from 'typeorm'
+import {
+  Check,
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  Unique,
+} from 'typeorm'
 
 @Entity()
 @Unique(['meeting', 'order'])
+@Index(['meeting'], { unique: true, where: '"is_selected" = true' })
 @Check(`length("name") >= 1`)
 export class CourseCandidate extends BaseEntity {
   @ManyToOne(() => Meeting, { nullable: false, onDelete: 'CASCADE' })
